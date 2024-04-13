a4c727b6a437df2e6795f9d8af2347f70499351c
KevinPrisco <kevinprisco.ch@gmail.com>
2024-04-12 21:25:34 -0500

    Update: mejoras a las rutas usando el servicio de autenticacion

--- src/Routers/studentRoutes.py
+++ src/Routers/studentRoutes.py
@@ -23,18 +23,25 @@ router  = APIRouter(
 )
 
 @router.get("/getID/{id_student}", response_model= schemes.studentBase)
-def get_students(id_student: int, db: Session = Depends(get_db)):
-    #token: Annotated[str, Depends(Auth.oauth2_scheme)],
-    try:
-        estudiantes = studentsController.getStudentById(db, id_student)
-        return estudiantes
-    except NoResultFound as e:
-        message = str(e)
-        detail = {
-            "Status": 404,
-            "Message": message
-        }
-        return JSONResponse(status_code=404, content=detail)
+def get_students(token: Annotated[str, Depends(Auth.oauth2_scheme)], id_student: int, db: Session = Depends(get_db)):
+    user = token
+    if not user:
+        raise HTTPException(
+            status_code=401,
+            detail="Invalid authentication credentials",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+    else:
+        try:
+            estudiantes = studentsController.getStudentById(db, id_student)
+            return estudiantes
+        except NoResultFound as e:
+            message = str(e)
+            detail = {
+                "Status": 404,
+                "Message": message
+            }
+            return JSONResponse(status_code=404, content=detail)
     
 
 
@@ -63,44 +70,68 @@ def get_students(token: Annotated[str, Depends(Auth.oauth2_scheme)], db: Session
 
 
 @router.post("/create", response_model=schemes.studentBase)
-def create_students(student: schemes.studentBase, db: Session = Depends(get_db)):
-    try:
-        estudiantes = studentsController.createStudent(db, _student = student)
-        return estudiantes
-    except BaseException as e:
-        message = str(e)
-        detail = {
-            "Status": 400,
-            "Message": message
-        }
-        return JSONResponse(status_code=400, content=detail)
+def create_students(token: Annotated[str, Depends(Auth.oauth2_scheme)], student: schemes.studentBase, db: Session = Depends(get_db)):
+    user = token
+    if not user:
+        raise HTTPException(
+            status_code=401,
+            detail="Invalid authentication credentials",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+    else:
+        try:
+            estudiantes = studentsController.createStudent(db, _student = student)
+            return estudiantes
+        except BaseException as e:
+            message = str(e)
+            detail = {
+                "Status": 400,
+                "Message": message
+            }
+            return JSONResponse(status_code=400, content=detail)
 
 
 @router.put("/update", response_model=schemes.studentBase)
-def update_students(student: schemes.studentBase, db: Session = Depends(get_db)):
-    try:
-        estudiantes = studentsController.updateStudent(db, _student = student)
-        return estudiantes
-    
-    except BaseException as e:
-        message = str(e)
-        detail = {
-            "Status": 400,
-            "Message": message
-        }
-        return JSONResponse(status_code=400, content=detail)
+def update_students(token: Annotated[str, Depends(Auth.oauth2_scheme)],student: schemes.studentBase, db: Session = Depends(get_db)):
+    user = token
+    if not user:
+        raise HTTPException(
+            status_code=401,
+            detail="Invalid authentication credentials",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+    else:
+        try:
+            estudiantes = studentsController.updateStudent(db, _student = student)
+            return estudiantes
+        
+        except BaseException as e:
+            message = str(e)
+            detail = {
+                "Status": 400,
+                "Message": message
+            }
+            return JSONResponse(status_code=400, content=detail)
 
 
 @router.delete("/delete/{id_student}")
-def delete_students(id_student: int,db: Session = Depends(get_db)):
-    try: 
-        estudiantes = studentsController.deleteStudent(db, id_student)
-        return estudiantes
-    
-    except BaseException as e:
-        message = str(e)
-        detail = {
-            "Status": 400,
-            "Message": message
-        }
-        return JSONResponse(status_code=400, content=detail)
+def delete_students(token: Annotated[str, Depends(Auth.oauth2_scheme)], id_student: int,db: Session = Depends(get_db)):
+    user = token
+    if not user:
+        raise HTTPException(
+            status_code=401,
+            detail="Invalid authentication credentials",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+    else:
+        try: 
+            estudiantes = studentsController.deleteStudent(db, id_student)
+            return estudiantes
+        
+        except BaseException as e:
+            message = str(e)
+            detail = {
+                "Status": 400,
+                "Message": message
+            }
+            return JSONResponse(status_code=400, content=detail)
